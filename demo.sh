adce48d185f6feeb21889ac89f208c347c209f69
shanebacklight <shanexuanyili@hotmail.com>
2019-01-13 18:33:38 -0500

    command demo to test input files

---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./count example1 "Sly Fox" example1out       
+./count example1 the example1out
+./count example2 tomato example2out
+./count example2 potato example2out
+./count example3 the out5
+./count example3 "I want" out6
+./count example3 "I want to" out7
+./count example4 $'f\xA' out8
